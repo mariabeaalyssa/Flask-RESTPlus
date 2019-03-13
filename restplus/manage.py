e77a80e7be9c88a3b5e1a35d7639988473f60afe
mariabeaalyssa <beaalyssa1@gmail.com>
2019-03-14 00:26:35 +0800

    Security and Authentication

--- restplus/manage.py
+++ restplus/manage.py
@@ -8,6 +8,8 @@ from app.main import create_app, db
 from app.main.model import user
 from app import blueprint
 
+from app.main.model import blacklist
+
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
 app.app_context().push()
